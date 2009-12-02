19331bca3d86bda6757e88ca322c4b0089354622
Ajay Maurya <jeet.ajay@gmail.com>
2009-12-02 12:35:11 +0530

    updated init.rb

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
 
-require 'action_web_service'
+
 
 lib_path = File.join(directory, 'lib', 'apis')
-Dependencies.load_paths << lib_path
+ ActiveSupport::Dependencies.load_paths << lib_path
 
-Dependencies.load_once_paths.delete File.dirname(__FILE__) + "/app/controllers"
+ ActiveSupport::Dependencies.load_once_paths.delete File.dirname(__FILE__) + "/app/controllers"
 
 
 
- 
